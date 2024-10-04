ea80bd07e6e12812f1ca4160866539387df48b9a
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-04 12:48:25 +0200

    snack3

---
+++ snack-3/snack3.php
@@ -0,0 +1,47 @@
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack3</title>
+</head>
+<body>
+    <main>
+        <ul>
+            <?php for ( $i = 0; $i < count($db["teachers"]); $i++){ ?>
+                <li>
+                    <?= ?>
+                </li>
+            <?php}?>
+
+            
+        </ul>
+    </main>
+</body>
+</html>
